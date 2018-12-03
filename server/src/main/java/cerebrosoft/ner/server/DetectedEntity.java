3def703ceb4b290f75ed57b222069fbca02278d0
Brian Young <cerebrosoft@gmail.com>
2018-12-02 22:08:04 -0500

    fix for bad properties in DetectedEntity

--- server/src/main/java/cerebrosoft/ner/server/DetectedEntity.java
+++ server/src/main/java/cerebrosoft/ner/server/DetectedEntity.java
@@ -4,15 +4,15 @@ public class DetectedEntity {
     private final String name;
     private final String matchedText;
     private final String type;
-    private final int start;
     private final int offset;
+    private final int length;
 
-    public DetectedEntity(String name, String matchedText, String type, int start, int offset) {
+    public DetectedEntity(String name, String matchedText, String type, int offset, int length) {
         this.name = name;
         this.matchedText = matchedText;
         this.type = type;
-        this.start = start;
         this.offset = offset;
+        this.length = length;
     }
 
     public String getName() {
@@ -27,12 +27,12 @@ public class DetectedEntity {
         return type;
     }
 
-    public int getStart() {
-        return start;
-    }
-
     public int getOffset() {
         return offset;
+    }
+
+    public int getLength() {
+        return length;
     }
 
 }
